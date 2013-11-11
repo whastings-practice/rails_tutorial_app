82b38a557ee0bbef358275f64a1c8098c9260b24
Will Hastings <will@kalamuna.com>
2013-11-10 17:14:30 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTutorialApp::Application.config.secret_token = '74d30456efcf2490cf7b966a20f3f212e38916bb97258f7c571d83350ed2a96eb92e53fc376de49a1b018dc0d8abda78153f1f525419625b77910288abcfe16f'
