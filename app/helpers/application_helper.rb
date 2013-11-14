3f05edc1d4082310e335600ee5a4a3c073de3e34
Will Hastings <will@kalamuna.com>
2013-11-13 20:01:36 -0800

    Add a title helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  BASE_TITLE = 'Rails Tutorial App'
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    return BASE_TITLE if page_title.empty?
+    "#{BASE_TITLE} | #{page_title}"
+  end
+
 end
