697ef174b9bb2b60284f2f008d389e3edb8bcc6e
Will Hastings <will@kalamuna.com>
2013-11-14 21:09:54 -0800

    Simplify static pages spec with shared_examples_for.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,13 +4,18 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: page_title) }
+  end
+
   describe "Home page" do
 
     before { visit root_path }
+    let(:heading) { 'Rails Tutorial App' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1', text: 'Rails Tutorial App') }
-
-    it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "all static pages"
 
     it { should_not have_selector('title', text: full_title('Home')) }
 
@@ -19,30 +24,30 @@ describe "Static pages" do
   describe "Help page" do
 
     before { visit help_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it { should have_selector('h1', text: 'Help') }
-
-    it { should have_selector('title', text: full_title('Help')) }
+    it_should_behave_like "all static pages"
 
   end
 
   describe "About page" do
 
     before { visit about_path }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About Us' }
 
-    it { should have_selector('h1', text: 'About Us') }
-
-    it { should have_selector('title', text: full_title('About Us')) }
+    it_should_behave_like "all static pages"
 
   end
 
   describe "Contact page" do
 
     before { visit contact_path }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_selector('h1', text: 'Contact') }
-
-    it { should have_selector('title', text: full_title('Contact')) }
+    it_should_behave_like "all static pages"
 
   end
 
