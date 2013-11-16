67f9151fd22bf155344faae038d2b1391df526c6
Will Hastings <will@kalamuna.com>
2013-11-16 13:20:24 -0800

    Make a basic User model.

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  # Validations:
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  # Callbacks:
+  before_save { |user| user.email.downcase! }
+
+end
